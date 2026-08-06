dd895a94e6ad4ee25b98cc0df0cf4c597f6f94de
Miguel de Almeida Alves <migueldealmeidaalves55@gmail.com>
2026-08-06 17:37:33 -0300

    Randomizer for short URLS.

---
+++ internal/handlers/short.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"crypto/rand"
+	"log"
+	"math/big"
+	"strings"
+)
+
+const (
+	defaultMaxShortURLSize = 5
+	chars                  = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+)
+
+func generateShortURL() (string, error) {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("Recovered from panic:", err)
+		}
+	}()
+
+	var sb strings.Builder
+	for range defaultMaxShortURLSize {
+		index, err := rand.Int(rand.Reader, big.NewInt(int64(len(chars)-1)))
+		if err != nil {
+			return "", err
+		}
+
+		sb.WriteByte(chars[index.Int64()])
+	}
+
+	return sb.String(), nil
+}
